3018e2903f7546c19d1c106419bc469b01f45e72
William Oktavianus <williamoktavianus@icloud.com>
2025-01-21 14:12:59 +0700

    Adding fetching catalogs

---
+++ service/catalogService.js
@@ -0,0 +1,25 @@
+export const fetchCatalogs = async () => {
+  try {
+    const url = "https://ams.akikita.web.id/api/filter/battery";
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (data.status != "success") {
+      throw new Error(data.message);
+    }
+
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
+};
