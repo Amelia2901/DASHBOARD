1c4ac8c33710e5d2d50c15f1cc7f35d89eb5037f
William Oktavianus <williamoktavianus@icloud.com>
2025-01-21 15:08:31 +0700

    Adding catalog detail

--- service/catalogService.js
+++ service/catalogService.js
@@ -23,3 +23,29 @@ export const fetchCatalogs = async () => {
     throw error;
   }
 };
+
+export const fetchCatalogDetails = async (batteryId) => {
+  try {
+    const url = `https://ams.akikita.web.id/api/filter/battery/${batteryId}`;
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (data.status != "success") {
+      throw new Error(data.message);
+    }
+
+    return data.data[0];
+  } catch (error) {
+    throw error;
+  }
+};
