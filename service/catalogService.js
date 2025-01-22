d530537ff1f14b846dd2aab0201704d8763f9d5a
William Oktavianus <williamoktavianus@icloud.com>
2025-01-22 13:41:46 +0700

    Adding fetching catalogs in item detail

--- service/catalogService.js
+++ service/catalogService.js
@@ -28,6 +28,33 @@ export const fetchCatalogs = async (limit = -1) => {
   }
 };
 
+export const fetchCatalogsByVehicleModel = async (vehicleModel) => {
+  try {
+    const url = `https://ams.akikita.web.id/api/filter/model/${vehicleModel}`;
+
+    console.log(url);
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (data.status != "success") {
+      throw new Error(data.message);
+    }
+
+    return data.data[0].batteries;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const fetchCatalogDetails = async (batteryId) => {
   try {
     const url = `https://ams.akikita.web.id/api/filter/battery/${batteryId}`;
